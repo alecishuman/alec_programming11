c06e3cb73dabdb4440e5ea57807ecb14540fac34
alecishuman <aasitu@gmail.com>
2021-11-18 14:52:31 -0800

    rps finished

--- rock-paper-scissors/src/Main.java
+++ rock-paper-scissors/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +10,12 @@ public class Main {
         System.out.print("Do you want to play?");
         String random = sc.nextLine();
         System.out.println("Okay, let's go!");
-        play();
+        while (!over) {
+            play();
+            System.out.println("Do you want to play again? yes or no");
+            playAgain();
+        }
+
     }
 
     public static void play() {
@@ -17,28 +23,34 @@ public class Main {
         int compNum = (int) Math.floor(Math.random() * 3);
         String[] choices = {"rock", "paper", "scissors"};
         System.out.println("1) rock 2) paper 3) scissors");
-        int num =0;
+        int num = -1;
         try {
             num = sc.nextInt() - 1;
         } catch (Exception ignore) {
-            System.out.println("Please choose from 1, 2, or 3. \n");
-            play();
         }
         if (!(num >= 0 && num <= 2)) {
             System.out.println("Please choose from 1, 2, or 3. \n");
             play();
-        } else if (!over) {
+        } else {
             System.out.println("The computer chose " + choices[compNum] + ". You chose " + choices[num] + ".");
             if (compNum == num) {
                 System.out.println("It's a draw!");
-                over = true;
             } else if ((compNum + 1) % 3 == (num % 3)) {
                 System.out.println("You win!");
-                over = true;
             } else {
                 System.out.println("You lose!");
-                over = true;
             }
+        }
+    }
+
+    public static void playAgain() {
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.nextLine();
+        if (!Objects.equals(answer, "yes") && !Objects.equals(answer, "no")) {
+            System.out.println("Please choose yes or no. \n");
+            playAgain();
+        } else if (answer.equals("no")) {
+            over = true;
         }
     }
 }
